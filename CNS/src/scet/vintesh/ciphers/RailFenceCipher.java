7db479f9e9291123cdf988b4919f541c28c37d38
Vintesh <vintesh89@gmail.com>
2013-03-02 15:56:55 +0530

    Railfence Cipher is completed. Now Transposition is remaining to be done

--- CNS/src/scet/vintesh/ciphers/RailFenceCipher.java
+++ CNS/src/scet/vintesh/ciphers/RailFenceCipher.java
@@ -4,24 +4,86 @@
  */
 package scet.vintesh.ciphers;
 
+import java.util.Arrays;
+import scet.vintesh.util.CharUtil;
+
 /**
  *
  * @author Vintesh
  */
 public class RailFenceCipher extends Cipher {
 
-    @Override
-    public void preProcessing() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    private int depth = 3;
 
     @Override
     public char[] getDecryptedData(char[] encryptedData) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int noOfCols = encryptedData.length / depth;
+        if (encryptedData.length % depth != 0) {
+            noOfCols++;
+        }
+
+        char[][] convertedJaggedArray = new char[depth][noOfCols];
+        int k = 0;
+        for (int j = 0; j < depth; j++) {
+            for (int i = 0; i < noOfCols; i++) {
+                if (k >= encryptedData.length) {
+                    break;
+                }
+                convertedJaggedArray[j][i] = encryptedData[k++];
+            }
+        }
+
+        char[] plainText = new char[encryptedData.length];
+        k = 0;
+        for (int i = 0; i < noOfCols; i++) {
+            for (int j = 0; j < depth; j++) {
+                if (k >= encryptedData.length) {
+                    break;
+                }
+                plainText[k++] = convertedJaggedArray[j][i];
+            }
+        }
+
+        return new String(plainText).toCharArray();
     }
 
     @Override
     public char[] getEncryptedData(char[] inputData) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        inputData = CharUtil.removeExceptAlphabates(inputData);
+
+        int noOfCols = (int) (inputData.length / depth);
+        if (inputData.length % depth != 0) {
+            noOfCols++;
+        }
+        char[][] convertedJaggedArray = new char[depth][noOfCols];
+        int k = 0;
+        for (int j = 0; j < noOfCols; j++) {
+            for (int i = 0; i < depth; i++) {
+                if (k >= inputData.length) {
+                    break;
+                }
+                convertedJaggedArray[i][j] = inputData[k++];
+            }
+        }
+
+        String st = "";
+        for (int i = 0; i < depth; i++) {
+            st += new String(convertedJaggedArray[i]);
+        }
+        return st.toCharArray();
+    }
+
+    @Override
+    public void preProcessing() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "===================================================== Rail Fence Cipher ===================================================================";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("CipherText: " + Arrays.toString(new RailFenceCipher().getDecryptedData(new RailFenceCipher().getEncryptedData("vintesh patelXY".toCharArray()))));
     }
 }
